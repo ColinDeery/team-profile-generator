664d423e639e81c71c572b2f74a34110afe3a1f6
ColinDeery <colinbdeery@gmail.com>
2022-12-09 09:26:08 -0500

    set up constructors and test suites

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,8 @@
+const Employee = require("../lib/Employee")
+
+describe('Employee', () => {
+    it('Should create a new instance of Employee', () => {
+        const employee = new Employee()
+        expect(employee).toBeInstanceOf(Employee)
+    })
+}) 
